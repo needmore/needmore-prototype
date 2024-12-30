0f33e493af934a7b75bbfe6b792205d3a617d673
Raymond Brigleb <ray@needmoredesigns.com>
2024-12-29 20:28:40 -0800

    Dark mode logo!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media',
   content: [
     "./src/**/*.{html,js}",
     "./docs/**/*.html",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      blue: colors.blue,
+      emerald: colors.emerald,
+      yellow: colors.yellow,
+    },
     extend: {
       fontFamily: {
         body: ['Futura-PT', 'Helvetica', 'Arial', 'sans-serif'],
